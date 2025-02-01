59f7bb4008048fabcee17f2c2cab3e83b0419092
KIRIESHKAFIR <keriafirsov2000@gmail.com>
2025-02-01 12:29:34 +0300

    update

--- Практические занятия/Практическое занятие 7/PZ7.2.py
+++ Практические занятия/Практическое занятие 7/PZ7.2.py
@@ -1,11 +1,10 @@
 # Дана строка, содержащая по крайней мере один символ пробела. Вывести подстроку,
 # расположенную между первым и последним пробелом исходной строки. Если
 # строка содержит только один пробел, то вывести пустую строку.
-sptr = "p sju r"
+sptr = "p sfnf mgfnfgm fkgj fgjkgju r"
 probel1 = sptr.index(' ')
-count_probel = sptr.count(" ")
-probel2 = sptr.index(' ',count_probel)
-if probel1 == probel2:
+possprobel =len(sptr) - sptr[::-1].index(" ")
+if probel1 == possprobel:
     print("")
 else:
-    print(sptr[probel1:probel2])
+    print(sptr[probel1:possprobel])
