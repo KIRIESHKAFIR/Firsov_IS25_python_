b52001888b0c2c56064203cbb74db543df2734b0
KIRIESHKAFIR <keriafirsov2000@gmail.com>
2025-02-01 11:55:13 +0300

    9

--- Практические занятия/Практическое занятие № 9/PZ.9.1.py
+++ Практические занятия/Практическое занятие № 9/PZ.9.1.py
@@ -2,5 +2,5 @@
 russ_eng = {"football": "футбол", "basketball": "баскетбол", "hockey": 'хоккей'
             , "volleyball": "воллейбол", "biathlon": "биатлон", "computer": "компьютер"
             , "joystick": "джостик", 'printer': 'принтер', 'laser': 'лазер', 'telephone': 'телефон'}
-trans = input("Введите слово на английском с маленькой буквы:")
+trans = input("Введите слово на английском с маленькой буквы: ")
 print(russ_eng.get(trans))
